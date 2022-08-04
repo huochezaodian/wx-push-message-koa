e8a692ccb11e32a2642ddf5bb0cd9f6cf0537a88
yangtianyi.number1 <yangtianyi.number1@bytedance.com>
2022-08-04 13:47:55 +0800

    update

--- config.js
+++ config.js
@@ -4,5 +4,6 @@ module.exports = {
   grant_type: 'client_credential',
   appid: 'wxfc8d4bf31335a16e',
   secret: 'c24ee9925a3efd3f106e83674fcccc75',
+  temlId77: 'feFqwCcYnpPPBxgNwcHP7bV9X_QlkSdCUkmjM7tyZiI',
   openids: ['ohZhj6qyFroznS3pi2naHfD6iJa8', 'ohZhj6vKGOWb2kwP1A1DE6evu-Ug']
 }
